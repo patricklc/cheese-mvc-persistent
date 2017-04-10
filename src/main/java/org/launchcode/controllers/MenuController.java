38bfa2b120954a696673a32cf61112659f0b6679
Patrick Short <pjsy8c@gmail.com>
2017-04-10 13:29:50 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,29 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Patrick on 4/10/2017.
+ */
+@Controller
+@RequestMapping(value="menu")
+public class MenuController {
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @RequestMapping(value="")
+    public String index(Model model) {
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "Menus");
+        return "menu/index";
+    }
+}
---
+++ src/main/java/org/launchcode/models/data/MenuDao.java
@@ -0,0 +1,15 @@
+package org.launchcode.models.data;
+
+import org.launchcode.models.Menu;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by Patrick on 4/10/2017.
+ */
+@Repository
+@Transactional
+public interface MenuDao extends CrudRepository<Menu, Integer> {
+}
